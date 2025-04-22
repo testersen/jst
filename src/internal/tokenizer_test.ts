00a80af0d42eec79cfe872c8189c8c74480adfc9
James Bradlee <james.bradlee@telenor.no>
2025-04-22 18:03:44 +0200

    test: add comprehensive tests for flushBuffer function to validate token addition and cursor position

--- src/internal/tokenizer_test.ts
+++ src/internal/tokenizer_test.ts
@@ -3,11 +3,14 @@ import {
   assertExists,
   assertInstanceOf,
   assertNotStrictEquals,
+  assertStrictEquals,
 } from "@std/assert";
 
 import {
   createState,
+  flushBuffer,
   flushRange,
+  InterpolationMode,
   type LiteralMode,
   Mode,
   trackCharacter,
@@ -16,6 +19,8 @@ import {
   LocationSnapshot,
   LocationTracker,
   type RangeWithLocation,
+  type Token,
+  TokenType,
 } from "./common.ts";
 
 /**
@@ -23,11 +28,11 @@ import {
  * properties of a {@link RangeWithLocation} object.
  */
 interface ExpectedRange {
-    start?: number;
-    end?: number;
-    length?: number;
-    startLocation?: { line?: number; column?: number };
-    endLocation?: { line?: number; column?: number };
+  start?: number;
+  end?: number;
+  length?: number;
+  startLocation?: { line?: number; column?: number };
+  endLocation?: { line?: number; column?: number };
 }
 
 /**
@@ -394,6 +399,315 @@ Deno.test("flushRange(state)", async (t) => {
           endLocation: { line: 2, column: 3 },
         },
       );
+    },
+  );
+});
+
+Deno.test("flushBuffer(state, tokens, type", async (t) => {
+  await t.step("empty buffer does not add tokens", async (t) => {
+    const state = createState() as LiteralMode;
+
+    await assertRange(
+      t,
+      "state has default cursor position",
+      state.locationTracker.complete(state.locationSnapshot),
+      {
+        start: 0,
+        end: 0,
+        length: 0,
+        startLocation: { line: 1, column: 0 },
+        endLocation: { line: 1, column: 0 },
+      },
+    );
+
+    const tokens: Token[] = [];
+
+    flushBuffer(state, tokens, TokenType.Literal);
+
+    await t.step(
+      "no tokens were added",
+      () => assertStrictEquals(tokens.length, 0, "token array should be empty"),
+    );
+
+    await assertRange(
+      t,
+      "state still has default cursor position",
+      state.locationTracker.complete(state.locationSnapshot),
+      {
+        start: 0,
+        end: 0,
+        length: 0,
+        startLocation: { line: 1, column: 0 },
+        endLocation: { line: 1, column: 0 },
+      },
+    );
+  });
+
+  await t.step("interpolation token is added from flush buffer", async (t) => {
+    const VALUE = "foobar";
+
+    const locationTracker = new LocationTracker();
+    const state = {
+      locationTracker,
+      locationSnapshot: locationTracker.snapshot(),
+      type: Mode.Interpolation,
+      buffer: "",
+      n: 1,
+    } satisfies InterpolationMode;
+
+    state.buffer = VALUE;
+
+    await assertRange(
+      t,
+      "state has default cursor position",
+      state.locationTracker.complete(state.locationSnapshot),
+      {
+        start: 0,
+        end: 0,
+        length: 0,
+        startLocation: { line: 1, column: 0 },
+        endLocation: { line: 1, column: 0 },
+      },
+    );
+
+    const tokens: Token[] = [];
+
+    flushBuffer(state, tokens, TokenType.Literal);
+
+    await t.step(
+      "tokens array should have 1 token",
+      () => assertStrictEquals(tokens.length, 1, "token length should be 1"),
+    );
+
+    const token = tokens[0];
+
+    await t.step("token type should be Literal", () => {
+      assertExists(
+        token,
+        "Token should have been added by flushBuffer(), but wasn't",
+      );
+
+      assertStrictEquals(
+        token.type,
+        TokenType.Literal,
+        `Expected token type to be Literal, but was ${TokenType[token.type]}`,
+      );
+    });
+
+    await t.step(`token value should be ${VALUE}`, () => {
+      assertExists(
+        token.value,
+        "Token should have been added by flushBuffer(), but wasn't",
+      );
+
+      assertStrictEquals(token.value, VALUE);
+    });
+
+    // We haven't moved the cursor using state.locationTracker
+    // so we still expect the start and end to be 0
+    await assertRange(
+      t,
+      "state still has default cursor position",
+      state.locationTracker.complete(state.locationSnapshot),
+      {
+        start: 0,
+        end: 0,
+        length: 0,
+        startLocation: { line: 1, column: 0 },
+        endLocation: { line: 1, column: 0 },
+      },
+    );
+  });
+
+  await t.step("literal token is added from flush buffer", async (t) => {
+    const VALUE = "foobar";
+
+    const state = createState() as LiteralMode;
+
+    state.buffer = VALUE;
+
+    await assertRange(
+      t,
+      "state has default cursor position",
+      state.locationTracker.complete(state.locationSnapshot),
+      {
+        start: 0,
+        end: 0,
+        length: 0,
+        startLocation: { line: 1, column: 0 },
+        endLocation: { line: 1, column: 0 },
+      },
+    );
+
+    const tokens: Token[] = [];
+
+    flushBuffer(state, tokens, TokenType.Interpolation);
+
+    await t.step(
+      "tokens array should have 1 token",
+      () => assertStrictEquals(tokens.length, 1, "token length should be 1"),
+    );
+
+    const token = tokens[0];
+
+    await t.step("token type should be Interpolation", () => {
+      assertExists(
+        token,
+        "Token should have been added by flushBuffer(), but wasn't",
+      );
+
+      assertStrictEquals(
+        token.type,
+        TokenType.Interpolation,
+        `Expected token type to be Interpolation, but was ${
+          TokenType[token.type]
+        }`,
+      );
+    });
+
+    await t.step(`token value should be ${VALUE}`, () => {
+      assertExists(
+        token.value,
+        "Token should have been added by flushBuffer(), but wasn't",
+      );
+
+      assertStrictEquals(token.value, VALUE);
+    });
+
+    // We haven't moved the cursor using state.locationTracker
+    // so we still expect the start and end to be 0
+    await assertRange(
+      t,
+      "state still has default cursor position",
+      state.locationTracker.complete(state.locationSnapshot),
+      {
+        start: 0,
+        end: 0,
+        length: 0,
+        startLocation: { line: 1, column: 0 },
+        endLocation: { line: 1, column: 0 },
+      },
+    );
+  });
+
+  await t.step(
+    "flushBuffer() attaches correct location range to token",
+    async (t) => {
+      const FLUSHES: {
+        value: string;
+        expectedRange: ExpectedRange;
+      }[] = [
+        {
+          value: "foobar",
+          expectedRange: {
+            start: 0,
+            end: 6,
+            length: 6,
+            startLocation: { line: 1, column: 0 },
+            endLocation: { line: 1, column: 6 },
+          },
+        },
+        {
+          value: "foo\r\nbar",
+          expectedRange: {
+            start: 6,
+            end: 14,
+            length: 8,
+            startLocation: { line: 1, column: 6 },
+            endLocation: { line: 2, column: 3 },
+          },
+        },
+        {
+          value: "\r\r\r\n\r\r\r",
+          expectedRange: {
+            start: 14,
+            end: 21,
+            length: 7,
+            startLocation: { line: 2, column: 3 },
+            endLocation: { line: 3, column: 0 },
+          },
+        },
+        {
+          value: "\n\n\n",
+          expectedRange: {
+            start: 21,
+            end: 24,
+            length: 3,
+            startLocation: { line: 3, column: 0 },
+            endLocation: { line: 6, column: 0 },
+          },
+        },
+        {
+          value: "\\hello",
+          expectedRange: {
+            start: 24,
+            end: 30,
+            length: 6,
+            startLocation: { line: 6, column: 0 },
+            endLocation: { line: 6, column: 6 },
+          },
+        },
+      ];
+
+      const state = createState() as LiteralMode;
+      const tokens: Token[] = [];
+
+      for (let i = 0; i < FLUSHES.length; i++) {
+        const { value, expectedRange } = FLUSHES[i];
+
+        state.buffer += value;
+        for (const char of value) {
+          trackCharacter(state, char);
+        }
+
+        flushBuffer(state, tokens, TokenType.Literal);
+
+        await t.step(`flush buffer case ${i}`, async (t) => {
+          assertStrictEquals(
+            tokens.length,
+            i + 1,
+            `tokens length should be ${i + 1}`,
+          );
+
+          const token = tokens[i];
+
+          await t.step("token type should be Literal", () => {
+            assertExists(
+              token,
+              "Token should have been added by flushBuffer(), but wasn't",
+            );
+
+            assertStrictEquals(
+              token.type,
+              TokenType.Literal,
+              `Expected token type to be Literal, but was ${
+                TokenType[token.type]
+              }`,
+            );
+          });
+
+          await t.step(
+            `token value should be ${
+              value.replaceAll("\n", "\\n").replaceAll("\r", "\\r")
+            }`,
+            () => {
+              assertExists(
+                token.value,
+                "Token should have been added by flushBuffer(), but wasn't",
+              );
+
+              assertStrictEquals(token.value, value);
+            },
+          );
+
+          await assertRange(
+            t,
+            "token range should match expected range",
+            token.range,
+            expectedRange,
+          );
+        });
+      }
     },
   );
 });
