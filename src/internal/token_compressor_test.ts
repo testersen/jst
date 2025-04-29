898d9597c4ec2d88c85500e649a78aad0fce876a
James Bradlee <james.bradlee@telenor.no>
2025-04-30 01:44:50 +0200

    test(token_compressor): add tests for TokenCompressorStream functionality

--- src/internal/token_compressor_test.ts
+++ src/internal/token_compressor_test.ts
@@ -93,8 +93,90 @@ Deno.test("compressTokens(tokens)", async (t) => {
   ];
 
   for (let i = 0; i < results.length; i++) {
-    await t.step(`token ${i}`, async () => {
+    await t.step(`token ${i}`, async (t) => {
       await assertToken(t, tokens[i], results[i]);
     });
   }
+});
+
+Deno.test("new TokenCompressorStream()", async (t) => {
+  const chunks = tokenize("foo\\bar\\baz{}\\{monday{tuesday}}{foo}{bar}baz");
+
+  const results: ExpectedToken[] = [
+    {
+      type: TokenType.Literal,
+      value: "foo\\bar\\baz",
+      start: 0,
+      end: 11,
+    },
+    {
+      type: TokenType.Literal,
+      value: "{monday",
+      start: 14,
+      end: 21,
+    },
+    {
+      type: TokenType.Interpolation,
+      value: "tuesday",
+      start: 22,
+      end: 29,
+    },
+    {
+      type: TokenType.Literal,
+      value: "}",
+      start: 30,
+      end: 31,
+    },
+    {
+      type: TokenType.Interpolation,
+      value: "foo",
+      start: 32,
+      end: 35,
+    },
+    {
+      type: TokenType.Interpolation,
+      value: "bar",
+      start: 37,
+      end: 40,
+    },
+    {
+      type: TokenType.Literal,
+      value: "baz",
+      start: 41,
+      end: 44,
+    },
+  ];
+
+  const stream = new TokenCompressorStream();
+
+  const writer = stream.writable.getWriter();
+
+  const awaitPromises: Promise<void>[] = [];
+
+  for (const chunk of chunks) {
+    awaitPromises.push(writer.write(chunk));
+  }
+
+  awaitPromises.push(writer.close());
+
+  let i = 0;
+
+  for await (const token of stream.readable) {
+    const currentToken = i++;
+    const expected = results.shift();
+
+    if (expected === undefined) {
+      throw new Error(`No token expected at index ${currentToken}`);
+    }
+
+    await t.step(`token ${currentToken}`, async (t) => {
+      await assertToken(t, token, expected);
+    });
+  }
+
+  await Promise.all(awaitPromises);
+
+  await t.step("there should be no more results left", () => {
+    assertStrictEquals(results.length, 0);
+  });
 });
